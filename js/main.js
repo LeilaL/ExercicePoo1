ca41a5accf538ca7464dd61c35db3dfe344a6eb3
Leila.L <leila.lababsa@hotmail.fr>
2017-07-27 16:43:19 +0200

    exercie 1 :add methods

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-var user = {
-  prenom: "Martin",
-  nom: "Matin",
-  pays: "France",
-  age: 11,
-  paiement:["visa", "mastercard", "cb"],
-};
-
-alert("Je m'appelle" + " " + user.prenom);
-alert("Mon nom est" + " " + user.nom);
-alert("Je suis né en" + " " + (2017-user.age));
-
-var infos = {
-  paysAutorisés = ["FRANCE", "BELGIQUE", "SUISSE", "LUXEMBOURG"];
-  paimentAutorisés = ["VISA", "MASTERCARD", "CB", "PAYPAL"];
-  ageMin = 18;
-
-}
 // function user(prenom, nom, pays, age, annee, paiement) {
 //     this.prenom = first;
 //     this.nom = last;
@@ -31,3 +13,38 @@ var infos = {
 // var inconnu = new Person("Martin", "Matin", "France", 11, 2017);
 //
 // inconnu.getInfos();
+
+var user = {
+  prenom: "Martin",
+  nom: "Matin",
+  pays: "FRANCE",
+  age: 18,
+  paiement:["visa", "mastercard", "cb"],
+};
+
+alert("Je m'appelle" + " " + user.prenom);
+alert("Mon nom est" + " " + user.nom);
+alert("Je suis né en" + " " + (2017-user.age));
+var x;
+
+var infos = {
+  paysAutorisés: ["FRANCE", "BELGIQUE", "SUISSE", "LUXEMBOURG"],
+  paimentAutorisés: ["VISA", "MASTERCARD", "CB", "PAYPAL"],
+  ageMin: 18,
+  getAutorisation: function() {
+    if (user.age >= infos.ageMin) {
+      alert("Vous pouvez naviguer sur notre site");
+}
+
+for (x in infos.paysAutorisés){
+  alert("Pays Ok");
+}
+
+for (x in infos.paimentAutorisés){
+  alert("Pays Ok");
+}
+
+  }
+
+};
+infos.getAutorisation();
