09915692db565668c9c1275162dea3e661fe0530
Leila.L <leila.lababsa@hotmail.fr>
2017-08-23 11:04:31 +0200

    checked indent+coms

--- js/main.js
+++ js/main.js
@@ -1,86 +1,62 @@
-// function user(prenom, nom, pays, age, annee, paiement) {
-//     this.prenom = first;
-//     this.nom = last;
-//     this.pays = pays;
-//     this.age = age;
-//     this.annee = annee;
-//     this.paiement = paiement;
-//     this.getInfos = function() {
-//         alert(this.prenom);
-//     };
-// }
-//
-// var inconnu = new Person("Martin", "Matin", "France", 11, 2017);
-//
-// inconnu.getInfos();
 
 var user = {
-    prenom: "Martin",
-    nom: "Matin",
-    pays: "france".toUpperCase(),
-    age: 18,
-    paiement: ["visa", "cb", "paypal"],
-
-    // paiement.map : function(x) {
-    //    return x.toUpperCase() }
+  prenom: "Martin",
+  nom: "Matin",
+  pays: "france".toUpperCase(),
+  age: 18,
+  paiement: ["visa", "cb", "paypal"],
 
 
-    getName: function() {
-        alert("Je m'appelle" + " " + this.prenom);
-    },
-    getLastName: function() {
-        alert("Mon nom est" + " " + this.nom);
-    },
-    getAge: function() {
-        alert("Je suis né en" + " " + (2017 - this.age));
-    },
+  getName: function() {
+    alert("Je m'appelle" + " " + this.prenom);
+  },
+  getLastName: function() {
+    alert("Mon nom est" + " " + this.nom);
+  },
+  getAge: function() {
+    alert("Je suis né en" + " " + (2017 - this.age));
+  },
 };
 user.getName();
 user.getLastName();
 user.getAge();
 
-// toUpper = function(x){
-//   return x.toUpperCase();
-// };
-// user.paiement.map(toUpper);
-
 
 var infos = {
-    paysAutorisés: ["FRANCE", "BELGIQUE", "SUISSE", "LUXEMBOURG"],
-    paiementAutorisés: ["VISA", "MASTERCARD", "CB", "PAYPAL"],
-    ageMin: 18,
-    getAutorisation: function() {
-        if (user.age >= infos.ageMin) {
-            alert("Vous pouvez naviguer sur notre site");
-        } else {
-            alert("Vous êtes trop jeune");
-        }
-    },
-
-    verifPays: function(a) {
-        if (infos.paysAutorisés.indexOf(user.pays.toUpperCase()) !== -1) {
-            alert("Pays OK");
-        } else {
-            alert("pays non autorisé!")
-        }
-    },
-
-    verifPaiement: function(a) {
-        var paiOk = 0;
-        for (var i = 0; i < user.paiement.length; i++) {
-            user.paiement[i] = user.paiement[i].toUpperCase();
-            // $tabMaj = array_map('strtoupper', $user.psaiement);
-            if (infos.paiementAutorisés.includes(user.paiement[i])) {
-                paiOk++;
-                console.log(paiOk);
-            }
-        }
-        if (paiOk) {
-            alert("Paiement OK");
-        } else {
-            alert("Paiement non autorisé");
-        }
+  paysAutorisés: ["FRANCE", "BELGIQUE", "SUISSE", "LUXEMBOURG"],
+  paiementAutorisés: ["VISA", "MASTERCARD", "CB", "PAYPAL"],
+  ageMin: 18,
+  getAutorisation: function() {
+    if (user.age >= infos.ageMin) {
+      alert("Vous pouvez naviguer sur notre site");
+    } else {
+      alert("Vous êtes trop jeune");
     }
+  },
+
+  verifPays: function(x) {
+    if (infos.paysAutorisés.indexOf(user.pays.toUpperCase()) !== -1) {
+      alert("Pays OK");
+    } else {
+      alert("pays non autorisé!")
+    }
+  },
+
+  verifPaiement: function(a) {
+    var paiOk = 0;
+    for (var i = 0; i < user.paiement.length; i++) {
+      user.paiement[i] = user.paiement[i].toUpperCase();
+      if (infos.paiementAutorisés.includes(user.paiement[i])) {
+        paiOk++;
+        console.log(paiOk);
+      }
+    }
+    if (paiOk) {
+      alert("Paiement OK");
+    } else {
+      alert("Paiement non autorisé");
+    }
+  }
 };
 infos.getAutorisation();
 infos.verifPays();
